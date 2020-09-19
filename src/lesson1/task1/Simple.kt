981d74b00568822b16843bf44204a5ee375c1aad
MalackAmbre <70660511+MalackAmbre@users.noreply.github.com>
2020-09-19 22:11:35 +0300

    Update Simple.kt

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -103,7 +103,8 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x2 - x1) + sqr(y2 - y1))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = 
+sqrt(sqr(x2 - x1) + sqr(y2 - y1))
 
 /**
  * Простая (2 балла)
@@ -133,7 +134,11 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    val x1 = initial + percent * initial / 100
+    val x2 = x1 + percent * x1 / 100
+    return percent * x2 * 0.01 + x2
+}
 
 /**
  * Простая (2 балла)
