55e81d0212c4a57db04da155611a9267215fccc1
MalackAmbre <70660511+MalackAmbre@users.noreply.github.com>
2020-09-27 20:17:10 +0300

    Update Logical.kt

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,8 +3,8 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
-import java.lang.Integer.max
-import java.lang.Integer.min
+import kotlin.math.abs
+import kotlin.math.max
 import kotlin.math.abs
 import kotlin.math.sqrt
 
@@ -44,12 +44,10 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int {
-    return when (month) {
-        1, 3, 5, 7, 8, 10, 12 -> 31
-        4, 6, 9, 11 -> 30
-        else -> if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) 29 else 28
-    }
+fun daysInMonth(month: Int, year: Int): Int = when (month) {
+    1, 3, 5, 7, 8, 10, 12 -> 31
+    4, 6, 9, 11 -> 30
+    else -> if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) 29 else 28
 }
 
 /**
@@ -62,7 +60,7 @@ fun daysInMonth(month: Int, year: Int): Int {
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = sqrt(sqr(x1 - x2) + sqr(y1 - y2)) <= (r2 - r1)
+): Boolean = sqrt(sqr(x1 - x2) + sqr(y1 - y2)) <= r2 - r1
 
 /**
  * Средняя (3 балла)
