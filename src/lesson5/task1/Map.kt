8eadb0387aef5de0bf6528530857d4f3b88f8558
MalackAmbre <70660511+MalackAmbre@users.noreply.github.com>
2020-11-06 22:33:25 +0300

    Update Map.kt

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -226,7 +226,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
 fun canBuildFrom(chars: List<Char>, word: String): Boolean {
-    for (char in word) {
+    for (char in word.toSet()) {
         if (char.toLowerCase() !in chars.map { it.toLowerCase() }) return false
     }
     return true
