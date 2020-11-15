dc8e89b150bb10c481bd232599193f9296b85bad
MalackAmbre <70660511+MalackAmbre@users.noreply.github.com>
2020-11-15 23:43:13 +0300

    Update List.kt

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -275,60 +275,67 @@ fun decimalFromString(str: String, base: Int): Int = TODO()
 fun roman(n: Int): String {
     var n = n
     var s = ""
-    while (n >= 1000) {
-        s += "M"
-        n -= 1000
-    }
-    while (n >= 900) {
-        s += "CM"
-        n -= 900
-    }
-    while (n >= 500) {
-        s += "D"
-        n -= 500
-    }
-    while (n >= 400) {
-        s += "CD"
-        n -= 400
-    }
-    while (n >= 100) {
-        s += "C"
-        n -= 100
-    }
-    while (n >= 90) {
-        s += "XC"
-        n -= 90
-    }
-    while (n >= 50) {
-        s += "L"
-        n -= 50
-    }
-    while (n >= 40) {
-        s += "XL"
-        n -= 40
-    }
-    while (n >= 10) {
-        s += "X"
-        n -= 10
-    }
-    while (n >= 9) {
-        s += "IX"
-        n -= 9
-    }
-    while (n >= 5) {
-        s += "V"
-        n -= 5
-    }
-    while (n >= 4) {
-        s += "IV"
-        n -= 4
-    }
     while (n >= 1) {
-        s += "I"
-        n -= 1
+        when {
+            n >= 1000 -> {
+                s += "M"
+                n -= 1000
+            }
+            n >= 900 -> {
+                s += "CM"
+                n -= 900
+            }
+            n >= 500 -> {
+                s += "D"
+                n -= 500
+            }
+            n >= 400 -> {
+                s += "CD"
+                n -= 400
+            }
+            n >= 100 -> {
+                s += "C"
+                n -= 100
+            }
+            n >= 90 -> {
+                s += "XC"
+                n -= 90
+            }
+            n >= 50 -> {
+                s += "L"
+                n -= 50
+            }
+            n >= 40 -> {
+                s += "XL"
+                n -= 40
+            }
+            n >= 10 -> {
+                s += "X"
+                n -= 10
+            }
+            n >= 9 -> {
+                s += "IX"
+                n -= 9
+            }
+            n >= 5 -> {
+                s += "V"
+                n -= 5
+            }
+            n >= 4 -> {
+                s += "IV"
+                n -= 4
+            }
+            n >= 1 -> {
+                s += "I"
+                n -= 1
+            }
+
+        }
+
     }
     return s
 }
+
 
 /**
  * Очень сложная (7 баллов)
